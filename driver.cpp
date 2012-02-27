02eba227a59db69a53afa3cac8ca5235c4d39590
Connor <glosser1@gmail.com>
2012-02-26 21:43:22 -0500

    Implemented basic FCC structure for testing (no boundary conditions)

--- driver.cpp
+++ driver.cpp
@@ -2,26 +2,40 @@
 #include "interaction.h"
 #include "integrator.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
-    Eigen::RowVector3d p0(0,0,0), p1(1,0,0);
-
     vector<Particle> object;
-    object.push_back(Particle(p0));
-    object.push_back(Particle(p1));
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            for(int k = 0; k < 5; k++) {
+                Eigen::RowVector3d p(i, j, k);
+                object.push_back(Particle(p));
+                object.push_back(Particle(p+Eigen::RowVector3d(0.0,0.5,0.5)));
+                object.push_back(Particle(p+Eigen::RowVector3d(0.5,0.0,0.5)));
+                object.push_back(Particle(p+Eigen::RowVector3d(0.5,0.5,0.0)));
+            }
+        }
+    }
+
+    for(vector<Particle>::iterator p = object.begin(); p != object.end();
+            p++) {
+          p->setPosition(p->position()*2);
+    }
 
     LennardJones lj(1,1);
 
     VerletIntegrator verlet(0.004, lj, object);
 
+    fstream output("positions.xyz", fstream::out);
 
     for(double time = 0; time < 10; time += 0.004) {
-       cout << object.size() << endl;
-       cout << "Time: " << time << endl;
+       output << object.size() << endl;
+       output << "Time: " << time << endl;
        for(vector<Particle>::iterator p = object.begin(); p != object.end();
                p++) {
-           cout << "Ar\t" << p->position() << endl;
+           output << "Ar\t" << p->position() << endl;
        }
        verlet.step();
     }
