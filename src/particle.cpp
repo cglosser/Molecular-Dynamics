02787126bd87b7ab3f770079a8afc6600187248f
Connor Glosser <glosser.ca@gmail.com>
2013-03-08 16:54:23 -0500

    Modified Particle constructors to use C++0x chaining

--- src/particle.cpp
+++ src/particle.cpp
@@ -4,65 +4,9 @@ using namespace std;
 /*=====Constructors===========================================*/
 
 /**
- * \brief   Default particle constructor
- * \details Sets all vectors to zero, mass to 1.
- */
-Particle::Particle() {
-    pos_ = vel_ = acc_ = Eigen::RowVector3d(0,0,0);
-    mass_ = 1.0;
-
-    return;
-}
-
-/**
- * \brief   Position constructor 
- * \details Only sets Particle position; velocity and acceleration assumed to
- *          equal zero, mass assumed to equal one.
- * \param position Vector object to assign to position
- */
-Particle::Particle(Eigen::RowVector3d position) {
-    pos_ = position;
-    vel_ = acc_ = Eigen::RowVector3d(0,0,0);
-    mass_ = 1.0;
-
-    return;
-}
-
-/**
- * \brief   Position & Mass constructor
- * \details Sets Particle position and mass via arguments, velocity and
- *          acceleration assumed zero.
- * \param position Vector object to assign to position
- * \param mass     Particle mass
- */
-Particle::Particle(Eigen::RowVector3d position, double mass) {
-    pos_ = position;
-    vel_ = acc_ = Eigen::RowVector3d(0,0,0);
-    mass_ = mass;
-
-    return;
-}
-
-/**
- * \brief   Vector constructor
- * \details Set all vector fields via arguments; mass assumed to equal 1.0.
- * \param position     Particle position vector
- * \param velocity     Particle velocity vector
- * \param Acceleration Particle acceleration vector
- */
-Particle::Particle(Eigen::RowVector3d position, Eigen::RowVector3d velocity, 
-        Eigen::RowVector3d acceleration) {
-    pos_ = position;
-    vel_ = velocity;
-    acc_ = acceleration;
-    mass_ = 1.0;
-
-    return;
-}
-
-/**
  * \brief   Full constructor
- * \details Set all Particle fields via arguments
+ * \details Set all Particle fields via arguments -- most general constructor
+ *          for use with C++0x constructor chaining.
  * \param position     Particle position vector
  * \param velocity     Particle velocity vector
  * \param acceleration Particle acceleration vector
@@ -77,6 +21,42 @@ Particle::Particle(Eigen::RowVector3d position, Eigen::RowVector3d velocity,
 
     return;
 }
+
+/**
+ * \brief   Vector constructor
+ * \details Set all vector fields via arguments; mass assumed to equal 1.0.
+ * \param position     Particle position vector
+ * \param velocity     Particle velocity vector
+ * \param Acceleration Particle acceleration vector
+ */
+Particle::Particle(Eigen::RowVector3d position, Eigen::RowVector3d velocity,
+        Eigen::RowVector3d acceleration) : Particle(position, velocity,
+            acceleration, 1.0) {}
+/**
+ * \brief   Position & Mass constructor
+ * \details Sets Particle position and mass via arguments, velocity and
+ *          acceleration assumed zero.
+ * \param position Vector object to assign to position
+ * \param mass     Particle mass
+ */
+Particle::Particle(Eigen::RowVector3d position, double mass) :
+    Particle(position, Eigen::RowVector3d(0,0,0), Eigen::RowVector3d(0,0,0),
+            mass) {}
+
+/**
+ * \brief   Position constructor 
+ * \details Only sets Particle position; velocity and acceleration assumed to
+ *          equal zero, mass assumed to equal one.
+ * \param position Vector object to assign to position
+ */
+Particle::Particle(Eigen::RowVector3d position) : Particle(position,
+        Eigen::RowVector3d(0,0,0), Eigen::RowVector3d(0,0,0), 1.0) {}
+/**
+ * \brief   Default particle constructor
+ * \details Sets all vectors to zero, mass to 1.
+ */
+Particle::Particle() : Particle(Eigen::RowVector3d(0,0,0),
+        Eigen::RowVector3d(0,0,0), Eigen::RowVector3d(0,0,0), 1.0) {}
 
 /*=====Mutators===============================================*/
 
